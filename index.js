df468b8bdbe252027598103d99440ac5ac01dc11
Jeff Hann <jeff.hann@redspace.com>
2015-06-27 23:42:08 -0300

    depending on the request there may or may not be a repository chunk to the payload, we now check for this

--- index.js
+++ index.js
@@ -16,12 +16,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-  var bbkeyName = req.body.repository.owner.login + "_token";
+  var org;
+
+
+  if(typeof(req.body.organization) !== 'undefined') {
+    org = req.body.organization.login;
+  } else {
+    org = req.body.repository.owner.login;
+  }
+
+  winston.log("debug", org);
+
+  var bbkeyName = org + "_token";
   bbkeyName = bbkeyName.replace(/-|\//g, "");
   bbkeyName = bbkeyName.toUpperCase();
 
+  winston.log("debug", bbkeyName);
+
   var config = {
-    owner: req.body.repository.owner.login,
+    owner: org,
     bbkey: process.env[bbkeyName],
     ghkey: process.env.GH_TOKEN,
     repo: req.body.repository.full_name,
@@ -30,41 +43,48 @@ app.post('/', function (req, res) {
     cwd: "gh"
   };
 
-  var err = null;
+  winston.log("debug", config);
 
-  Tasks.checkBitbucket(config)
-  .then(Tasks.gitClone)
-  .then(Tasks.fetchAll)
-  .then(Tasks.addMirror)
-  .then(Tasks.pushMirror)
-  .catch(function (e) {
-    err = e;
-    winston.error(e);
-  })
-  .done(function() {
-    //once were done cleanup and delete the downloaded code
-    var child = spawn(
-      "rm",
-      [
-        "-rf", 
-        config.cwd
-      ]
-    );
+  if (typeof(config.bbkey) == "undefined") {
+    winston.error("bbkey '" + bbkeyName + "' missing, please create one for bitbucket.com/" + org);
+    res.sendStatus(500);
+  } else {
+    var err = null;
 
-    child.stderr.on('data', function (data) {
-      err = new Error(data);
+    Tasks.checkBitbucket(config)
+    .then(Tasks.gitClone)
+    .then(Tasks.fetchAll)
+    .then(Tasks.addMirror)
+    .then(Tasks.pushMirror)
+    .catch(function (e) {
+      err = e;
+      winston.error(e);
+    })
+    .done(function() {
+      //once were done cleanup and delete the downloaded code
+      var child = spawn(
+        "rm",
+        [
+          "-rf", 
+          config.cwd
+        ]
+      );
+
+      child.stderr.on('data', function (data) {
+        err = new Error(data);
+      });
+
+      child.on('close', function (code) {
+        if (err === null) {
+          winston.log("debug", "deleted repo");
+          res.sendStatus(204);
+        } else {
+          winston.error(err);
+          res.sendStatus(500);
+        }
+      });
     });
-
-    child.on('close', function (code) {
-      if (err === null) {
-        winston.log("debug", "deleted repo");
-        res.sendStatus(204);
-      } else {
-        winston.error(err);
-        res.sendStatus(500);
-      }
-    });
-  });
+  }
 });
 
 app.listen(port, function () {
