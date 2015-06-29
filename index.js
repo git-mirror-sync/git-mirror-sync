86c27793edd435ab1173a814869d914e978a8b48
Jeff Hann <jeff.hann@redspace.com>
2015-06-29 14:12:12 -0300

    adding oauth 1

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // load the dependencies
+var request = require('request');
+var OAuth   = require('oauth');
 var express = require('express');
 var winston = require('winston');
 var bodyParser = require('body-parser');
@@ -18,9 +20,30 @@ app.use(bodyParser.json());
 app.get('/login', function(req, res) {
   winston.log("info", "login");
   var apiKey = process.env.BB_KEY;
+  var apiSecret = process.env.BB_SECRET;
 
-  if(typeof apiKey !== 'undefined') {
-    res.redirect("https://bitbucket.org/site/oauth2/authorize?client_id=" + apiKey + "&response_type=code");
+  if(typeof apiKey !== 'undefined' || typeof apiSecret !== 'undefined') {
+    var oauth = new OAuth.OAuth(
+      'https://bitbucket.org/api/1.0/oauth/request_token',
+      'https://bitbucket.org/!api/1.0/oauth/access_token',
+      apiKey,
+      apiSecret,
+      '1.0',
+      'https://git-mirror-sync.herokuapp.com/bbauth',
+      'HMAC-SHA1'
+    );
+
+    oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+      if (error) {
+        winston.log("error", error);
+        res.sendStatus("500");
+      } else {
+        winston.log("debug", oauth_token);
+        winston.log("debug", oauth_token_secret);
+
+        res.redirect("https://bitbucket.org/api/1.0/oauth/authenticate?oauth_token="+ oauth_token);
+      }
+    });
   } else {
     winston.error("missing bitbucket client_id expected enviroment variable BB_KEY");
     res.sendStatus(500);
@@ -30,7 +53,8 @@ app.get('/login', function(req, res) {
 app.get('/bbauth', function(req, res) {
   winston.log("info", "GET /bbauth");
 
-  var auth = req.query.code;
+  var auth = req.query.oauth_token;
+  var verifier = req.query.oauth_verifier;
 
   if (typeof auth !== 'undefined') {
       winston.log("debug", auth);
