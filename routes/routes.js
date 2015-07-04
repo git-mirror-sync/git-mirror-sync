b9151c6e4e5be8eb7069e8ef3f232fba983086d3
Jeff Hann <jeffhann@gmail.com>
2015-07-03 23:28:14 -0300

    removing commented out code

--- routes/routes.js
+++ routes/routes.js
@@ -30,31 +30,6 @@ module.exports = function(app, models, passport, tasks) {
     } else {
       res.render('profile');
     }
-
-    //var github = new GitHubApi({
-      //version: '3.0.0',
-      //protocol: 'https',
-      //host: 'api.github.com',
-      //timeout: 5000
-    //});
-
-    //github.authenticate({
-      //type: 'oauth',
-      //key: process.env.GH_ID,
-      //secret: process.env.GH_SECRET
-    //});
-
-    //github.user.get({
-      //user: req.user.username
-    //}, function(err, ghRes) {
-      //if (err) {
-        //winston.error(err);
-        //res.sendStatus(500);
-      //} else {
-        //winston.log('debug', JSON.stringify(ghRes));
-        //res.send(ghRes);
-      //}
-    //});
   });
 
   require("./auth")(app, models, passport);
