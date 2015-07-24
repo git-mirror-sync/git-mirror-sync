c6acd8776b08811b01cf20cbda7443207df1c34b
Jeff Hann <jeff.hann@redspace.com>
2015-07-24 15:12:37 -0300

    reversing logs array

--- routes/profile.js
+++ routes/profile.js
@@ -24,7 +24,7 @@ module.exports = function(app, models) {
         }
 
         var opts = {
-          logs: logs
+          logs: logs.reverse()
         };
 
         res.render('profile', opts);
